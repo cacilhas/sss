40a7a712e98bdf02ed4703c45e0fa292fc5b6202
Rodrigo Cacilhas <batalema@cacilhas.info>
2014-02-04 14:03:39 -0200

    bugfix: add wrap utils

--- src/wrap.c
+++ src/wrap.c
@@ -1,1 +1,40 @@
-../../wraputils/wrap.c
+#include <iso646.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "wrap.h"
+
+
+typedef struct {
+	void *wrapped;
+} wrapper_t;
+
+
+void table_setnumber(lua_State *L, const char *key, LUA_NUMBER value) {
+	lua_pushstring(L, key);
+	lua_pushnumber(L, value);
+	lua_settable(L, -3);
+}
+
+
+void table_setstring(lua_State *L, const char *key, const char *value) {
+	lua_pushstring(L, key);
+	lua_pushstring(L, value);
+	lua_settable(L, -3);
+}
+
+
+void wrap_push(lua_State *L, void *wrapped, const char *classname) {
+	wrapper_t *envelop = lua_newuserdata(L, sizeof(wrapper_t));
+	envelop->wrapped = wrapped;
+	luaL_getmetatable(L, classname);
+	lua_setmetatable(L, -2);
+}
+
+
+void *wrap_pull(lua_State *L, int index, const char *classname) {
+	wrapper_t *envelop = luaL_checkudata(L, index, classname);
+	if (envelop)
+		return envelop->wrapped;
+	return NULL;
+}
--- src/wrap.h
+++ src/wrap.h
@@ -1,1 +1,24 @@
-../../wraputils/wrap.h
+#ifndef _WRAP_H
+#define _WRAP_H
+
+/* copyright ©2014, Rodrigo Cacilhας <batalema@cacilhas.info> */
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void table_setnumber(lua_State *, const char *, LUA_NUMBER);
+void table_setstring(lua_State *, const char *, const char *);
+
+void wrap_push(lua_State *, void *, const char *);
+void *wrap_pull(lua_State *, int, const char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WRAP_H */
